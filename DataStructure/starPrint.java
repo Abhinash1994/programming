9fd9c993b4baee2584d2a48e1613b9600d7e35de
Abhinash1994 <pandit.bechu@gmail.com>
2019-02-04 23:30:37 +0530

    dd

---
+++ DataStructure/starPrint.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class starPrint{
+	
+	public static void main(String arg[])
+
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=n;j>=i;j--)
+			{
+				System.out.print(" ");
+
+			}
+			for(int k=1;k<=i;k++)
+			{
+				System.out.print(" *");
+			}
+			System.out.println("");
+		}
+	}
+}
